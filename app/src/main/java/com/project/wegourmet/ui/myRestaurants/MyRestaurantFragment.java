e2033858e825ed48bd99c18c9afe8f876bdd4e7f
Adi Papi <adipapismedov@gmail.com>
2022-03-22 06:27:40 +0200

    fix guest navigation bar icons

--- app/src/main/java/com/project/wegourmet/ui/myRestaurants/MyRestaurantFragment.java
+++ app/src/main/java/com/project/wegourmet/ui/myRestaurants/MyRestaurantFragment.java
@@ -5,16 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import com.project.wegourmet.R;
 import com.project.wegourmet.databinding.FragmentMyRestaurantsBinding;
-import com.project.wegourmet.ui.dashboard.DashboardViewModel;
 
 public class MyRestaurantFragment extends Fragment {
     private FragmentMyRestaurantsBinding binding;
@@ -22,14 +19,14 @@ public class MyRestaurantFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//        DashboardViewModel dashboardViewModel =
-//                new ViewModelProvider(this).get(DashboardViewModel.class);
+//        HistoryViewModel historyViewModel =
+//                new ViewModelProvider(this).get(HistoryViewModel.class);
 
         binding = FragmentMyRestaurantsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        final TextView textView = binding.textDashboard;
-//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textHistory;
+//        historyViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         addRestaurantBtn = root.findViewById(R.id.add_restaurant_btn);
 
